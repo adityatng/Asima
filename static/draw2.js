30c79b724ebd99c9e5e0bfef8f7006fe6b2557a6
adityatng <149ty1@gmail.com>
2022-11-29 21:54:22 +0700

    --

---
+++ static/draw2.js
@@ -0,0 +1,72 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+
+var app = document.getElementById('app');
+canvas.width = 250;
+canvas.height = 200;
+
+var mouse = {x: 0, y: 0};
+
+
+var digit, accuracy,pr
+    digit = document.getElementById('digit');
+    accuracy = document.getElementById('accuracy');
+    pr = document.getElementById('p');
+
+
+canvas.addEventListener('pointermove', function(e) {
+  mouse.x = e.pageX - this.offsetLeft;
+  mouse.y = e.pageY - this.offsetTop;
+}, false);
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.strokeStyle = 'black';
+
+canvas.addEventListener('pointerdown', function(e) {
+  mouse.x = e.pageX - this.offsetLeft;
+  mouse.y = e.pageY - this.offsetTop;
+  ctx.beginPath();
+  ctx.moveTo(mouse.x, mouse.y);
+  canvas.addEventListener('pointermove', onPaint, false);
+}, false);
+
+canvas.addEventListener('pointerup', function() {
+  canvas.removeEventListener('pointermove', onPaint, false);
+}, false);
+
+var onPaint = function() {
+  ctx.lineTo(mouse.x, mouse.y);
+  ctx.stroke();
+};
+
+function clearArea() {
+              // Use the identity matrix while clearing the canvas
+              ctx.setTransform(1, 0, 0, 1, 0, 0);
+              ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+              $('#digit').html('');
+              $('#accuracy').html('');
+              $('#p').html('');
+            }
+function save_image() {
+              const link = document.createElement('a');
+              var image = canvas.toDataURL();
+              console.log(image);
+              // clearArea();
+              $.ajax({
+                type: "POST",
+                url: "/predict",
+                data: {
+                  imageBase64: image
+                },
+                beforeSend: function() {
+                  pr.innerHTML = "Memproses masukan Anda...";
+
+                }
+              }).done(function(result) {
+                digit.innerHTML = result.character
+                accuracy.innerHTML = result.accuracy + '%'
+                pr.innerHTML = 'Hasil Prediksi'
+               
+              });
+            }
